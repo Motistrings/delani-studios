7d405c49d5e911657b4bb38d31f2c50eacdd4565
Motistrings <timgrand4929@gmail.com>
2020-04-12 09:02:27 +0300

    Customized the What To Do icons section to toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-  $("#des").click(function () {
+  $(".design-icon").click(function () {
+    $(".design-icon").hide(1200);
     $("#design").slideToggle("slow");
   });
-  $("#dev").click(function () {
+  $("#design").click(function () {
+    $("#design").hide(1200);
+    $(".design-icon").slideToggle("slow");
+    $(".design-icon").show();
+  });
+
+  $(".dev-icon").click(function () {
+    $(".dev-icon").hide(1200);
     $("#develop").slideToggle("slow");
   });
-  $("#prod").click(function () {
+  $("#develop").click(function () {
+    $("#develop").hide(1200);
+    $(".dev-icon").slideToggle("slow");
+  });
+
+  $(".prod-icon").click(function () {
+    $(".prod-icon").hide(1200);
     $("#product").slideToggle("slow");
+  });
+  $("#product").click(function () {
+    $("#product").hide(1200);
+    $(".prod-icon").slideToggle("slow");
   });
 });
