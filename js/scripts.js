ed7c2a732897ef3f9e8802019c72c6d59d7c551a
Motistrings <timgrand4929@gmail.com>
2020-04-12 07:40:54 +0300

    Added what we do section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#des").click(function () {
+    $("#design").slideToggle("slow");
+  });
+  $("#dev").click(function () {
+    $("#develop").slideToggle("slow");
+  });
+  $("#prod").click(function () {
+    $("#product").slideToggle("slow");
+  });
+});
