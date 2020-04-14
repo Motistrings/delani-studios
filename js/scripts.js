20629faa88176c812168d6c319df87c599d84304
Motistrings <timgrand4929@gmail.com>
2020-04-14 11:52:50 +0300

    Added pop-up jquery on submission

--- js/scripts.js
+++ js/scripts.js
@@ -99,3 +99,15 @@ $(document).ready(function () {
       $(".overlay8").hide();
     });
 });
+function sendMessage() {
+  var name = $("#NAME").val();
+  var email = $("#EMAIL").val();
+  var message = $("#MESSAGE").val();
+  if (name !== "" && email !== "" && message !== "") {
+    alert(
+      "Hey " +
+        name +
+        "! Thankyou,your message is being reviewed. Check your email for our feedback soon."
+    );
+  }
+}
