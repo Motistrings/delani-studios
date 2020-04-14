048bba43080f57a90c07806eab2030fe8d6630bf
Motistrings <timgrand4929@gmail.com>
2020-04-14 09:13:00 +0300

    Added overlays to my portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,75 @@ $(document).ready(function () {
     $(".prod-icon").slideToggle("slow");
   });
 });
+$(document).ready(function () {
+  $(".pic-1")
+    .mouseover(function () {
+      $(".overlay1").show();
+    })
+    .mouseout(function () {
+      $(".overlay1").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-2")
+    .mouseover(function () {
+      $(".overlay2").show();
+    })
+    .mouseout(function () {
+      $(".overlay2").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-3")
+    .mouseover(function () {
+      $(".overlay3").show();
+    })
+    .mouseout(function () {
+      $(".overlay3").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-4")
+    .mouseover(function () {
+      $(".overlay4").show();
+    })
+    .mouseout(function () {
+      $(".overlay4").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-5")
+    .mouseover(function () {
+      $(".overlay5").show();
+    })
+    .mouseout(function () {
+      $(".overlay5").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-6")
+    .mouseover(function () {
+      $(".overlay6").show();
+    })
+    .mouseout(function () {
+      $(".overlay6").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-7")
+    .mouseover(function () {
+      $(".overlay7").show();
+    })
+    .mouseout(function () {
+      $(".overlay7").hide();
+    });
+});
+$(document).ready(function () {
+  $(".pic-8")
+    .mouseover(function () {
+      $(".overlay8").show();
+    })
+    .mouseout(function () {
+      $(".overlay8").hide();
+    });
+});
